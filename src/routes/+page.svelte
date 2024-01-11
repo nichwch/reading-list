4079e24dec3a3a98a5475154b2fe630a99053654
nichwch <nichwch@gmail.com>
2024-01-10 23:54:33 -0500

    hello world example

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { invoke } from '@tauri-apps/api/tauri';
+
+	let message = '';
+</script>
+
+<h1>Welcome to your reading list!</h1>
+<button
+	on:click={async () => {
+		message = await invoke('greet', { name: 'nick' });
+	}}>press me</button
+>
+<div>{message}</div>
+<div>This was generated based off the content of the following files:</div>
+<div>The following topics were identified, which were</div>
