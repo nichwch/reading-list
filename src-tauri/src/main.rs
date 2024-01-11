f0537561a937d52c97db6bd5df11f39807fcda93
nichwch <nichwch@gmail.com>
2024-01-11 00:21:02 -0500

    wow rust is hard lol

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,29 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+use std::fs;
 
 fn main() {
-  tauri::Builder::default()
-    .invoke_handler(tauri::generate_handler![greet])
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![get_files])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
-
 
 #[tauri::command]
 fn greet(name: &str) -> String {
-   format!("Hello, {}!", name)
+    format!("Hello, {}!", name)
+}
+
+#[tauri::command]
+fn get_files(path: &str) -> Vec<String> {
+    let mut file_paths = Vec::new();
+    let paths = fs::read_dir(path).unwrap();
+    for entry in paths {
+        let entry = entry.unwrap();
+        if entry.path().is_file() {
+            file_paths.push(entry.path().to_string_lossy().to_string())
+        }
+    }
+    return file_paths;
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,17 @@
 <script lang="ts">
 	import { invoke } from '@tauri-apps/api/tauri';
+	import { onMount } from 'svelte';
 
-	let message = '';
+	let files: string[] = [];
+	onMount(async () => {
+		files = (await invoke('get_files', { path: '/Users/nichwch/notes' })) as string[];
+		console.log({ files });
+	});
 </script>
 
 <h1>Welcome to your reading list!</h1>
-<button
-	on:click={async () => {
-		message = await invoke('greet', { name: 'nick' });
-	}}>press me</button
->
-<div>{message}</div>
 <div>This was generated based off the content of the following files:</div>
+{#each files as file}
+	<div>{file}</div>
+{/each}
 <div>The following topics were identified, which were</div>
